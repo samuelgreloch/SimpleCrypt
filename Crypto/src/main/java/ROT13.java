63a1a9d9ddedc5d066c8e2a6c7f81bb51d4b72ed
sgreloch <samuelgreloch@yahoo.com>
2024-12-10 14:45:37 -0500

    need to fix pom

---
+++ Crypto/src/main/java/ROT13.java
@@ -0,0 +1,39 @@
+
+import static java.awt.SystemColor.text;
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+package com.zipcodewilmington.ROUT13;
+
+
+
+
+public class ROT13 {
+
+        ROT13(Character cs, Character cf) {
+        }
+
+        ROT13() {
+        }
+
+
+        public String crypt (String text) throws UnsupportedOperationException {
+
+            return "";
+        }
+
+        public String encrypt (String text){
+            return text;
+        }
+
+        public String decrypt (String text){
+            return text;
+        }
+
+        public static String rotate (String s, Character c){
+
+            return "";
+        }
+
+    }
+
